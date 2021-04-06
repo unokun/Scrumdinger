8d611acef9728fcad302e619a7a1cdbf6cd7e6b3
unokun <unokun@gmail.com>
2021-04-06 10:19:17 +0900

    Displaying Data in a List Section 1 Display a List of Daily Scrums

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Masaaki Uno on 2021/04/06.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
