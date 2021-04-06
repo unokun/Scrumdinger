40caf78977ea1feece06e3ca36b26f4aca0ea3a1
unokun <unokun@gmail.com>
2021-04-06 15:06:13 +0900

    Passing Data with Bindings Section 1 Pass the Edit View a Binding to Scrum Data

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct DetailView: View {
-    let scrum: DailyScrum
+    @Binding var scrum: DailyScrum
+    @State private var data: DailyScrum.Data = DailyScrum.Data()
     @State private var isPresented = false
     
     var body: some View {
@@ -45,16 +46,18 @@ struct DetailView: View {
         .listStyle(InsetGroupedListStyle())
         .navigationBarItems(trailing: Button("Edit") {
             isPresented = true
+            data = scrum.data
         })
         .navigationTitle(scrum.title)
         .fullScreenCover(isPresented: $isPresented) {
             NavigationView {
-                EditView()
+                EditView(scrumData: $data)
                     .navigationTitle(scrum.title)
                     .navigationBarItems(leading: Button("Cancel") {
                         isPresented = false
                     }, trailing: Button("Done") {
                         isPresented = false
+//                        scrum.update(from: data)
                     })
             }
         }
@@ -63,6 +66,8 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(scrum: DailyScrum.data[0])
+        NavigationView {
+            DetailView(scrum: .constant(DailyScrum.data[0]))
+        }
     }
 }
--- Scrumdinger/EditView.swift
+++ Scrumdinger/EditView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 
 struct EditView: View {
-    @State private var scrumData: DailyScrum.Data = DailyScrum.Data()
+    @Binding var scrumData: DailyScrum.Data
+//    @State private var scrumData: DailyScrum.Data = DailyScrum.Data()
     @State private var newAttendee = ""
     var body: some View {
         List {
@@ -54,6 +55,6 @@ struct EditView: View {
 
 struct EditView_Previews: PreviewProvider {
     static var previews: some View {
-        EditView()
+        EditView(scrumData: .constant(DailyScrum.data[0].data))
     }
 }
